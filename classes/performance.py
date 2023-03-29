47887978cb79b5261b9c2c0e32d4bf8512db3230
franzhopper <franzhopper@oban4.lan>
2023-03-29 22:52:37 +0200

    Module rework. rewrited from scratch

--- classes/performance.py
+++ classes/performance.py
@@ -1,13 +1,21 @@
-from classes import dbhandler
+from classes import riotrequester
+from classes.dbhandler import DBHandler
 from typing import Any
-from pprint import pprint
-import trueskill
+from trueskill import rate, Rating
+import keras
 import logging
 import numpy as np
 
 
-class MatchAnalyzer:
+def get_last_match_data_by_summoner_name(name) -> dict[str, Any]:
+    ritopls = riotrequester.RiotRequester()
+    summoner = ritopls.get_summoner_by_name(name)
+    match_ids = ritopls.get_matchlist_by_puuid(summoner['puuid'])
+    match_id = match_ids[0]
+    match = ritopls.get_match_by_match_id(match_id)
+    return match
 
+class Match:
     TO_EXTRACT = {
         'player_info': [
             'puuid',
@@ -27,141 +35,146 @@ class MatchAnalyzer:
             'saveAllyFromDeath',
             'immobilizeAndKillWithAlly',
             'killAfterHiddenWithAlly',
-            'knockEnemyIntoTeamAndKill',
-            ],
-        }
-     
-    def __init__(self, match_data) -> None:
-        self.log = logging.getLogger('Analyzer')
-        match_id = match_data['metadata']['matchId']
-        self.log.info(f'Initializing analyzer for {match_id}')
-        self.data = match_data
-        self.gamemode = match_data['info']['gameMode']
-        self.extracted = []
+            'knockEnemyIntoTeamAndKill',],}
 
-    def analyze(self):
-        self.extract_participants()
-        self.calculate_performance()
-        self.download_ratings()
-        self.rate_mmr()
-        self.rate_performance()
-        self.show_details()
-
-    def extract_participants(self):
-        self.log.info('Extracting Participants')
+    def __init__(self, match_data: dict[str, Any]) -> None:
+        self.log = logging.getLogger('Match')
+        self.log.info('Class initiated...')
+        self.data: dict[str, Any] = match_data
+        self.data_extracted: list[Any] = self._extract()
+        self.summoners: list[Summoner] = [
+                Summoner(row) for row in self.data_extracted]
+        self.model = keras.models.load_model('model_7828')
+        self._calculate_win_probability()
+        self._download_ratings()
+        self._rate_mmr()
+        self._rate_mvp()
+    
+    def _extract(self) -> list[Any]:
+        self.log.info('extracting summoners from given data')
+        x: list = []
         for value in self.TO_EXTRACT['player_info']:
-            row = [x[value] for x in self.data['info']['participants']]
-            self.extracted.append(row)
+            x.append([i[value] for i in self.data['info']['participants']])
         for value in self.TO_EXTRACT['statistics']:
-            row = [x[value] for x in self.data['info']['participants']]
-            self.extracted.append(row)
+            x.append(self._get_normalized_values(value))
         for value in self.TO_EXTRACT['challenges']:
-            try:
-                row = [x['challenges'][value] for x in self.data['info']['participants']]
-                self.extracted.append(row)
-            except Exception as e:
-                print('Missing statistic: ' + value)
-                continue
-        self.log.debug(self.extracted)
+            x.append(self._get_normalized_values(value, True))
+        return list(zip(*x))
 
-    def calculate_performance(self):
-        self.log.info('Calculating performance')
-        data = np.array(self.extracted[5:])
-        normalized = [(arr/np.linalg.norm(arr)) for arr in data if sum(arr)]
-        sumed = sum(normalized)
-        performance_score = sumed / np.linalg.norm(sumed)
-        self.extracted.append(performance_score)
-        self.log.debug(performance_score)
-
-    def download_ratings(self):
-        self.log.info('Downloading ratings')
-        db = dbhandler.DBHandler()
-        ratings = [db.get_rating(p) for p in self.data['info']['participants']]
-        del db
-        ratings = list(zip(*ratings))
-        for row in ratings:
-            self.extracted.append(row)
-
-    def rate_mmr(self):
-        x = list(zip(*self.extracted))
-        a, b = [],[]
-        for i in range(10):
-            m = x[i][-4]
-            s = x[i][-3]
-            if i<5:
-                a.append(trueskill.Rating(m, s))
+    def _get_normalized_values(self, value, challenge=False):
+        self.log.info('Normalizing...')
+        data = self.data['info']['participants']
+        try:
+            if challenge:
+                arr = [row['challenges'][value] for row in data]
             else:
-                b.append(trueskill.Rating(m, s))
-        if x[0][3]:
-            w = [0,1]
-        else:
-            w = [1,0]
+                arr = [row[value] for row in data]
+            y = np.array(arr)
+            x = y / np.linalg.norm(y)
+            if np.isnan(x[0]):
+                raise Exception
+            self.log.info(f'Returning normalized data: {x}')
+            return x 
+        except Exception as e:
+            self.log.info('Returning empty array due to unexpected error')
+            return np.array([0.0 for _ in range(10)])
 
-        rated = trueskill.rate([a,b],ranks=w)
-        m, s = [],[]
-        rated = rated[0] + rated[1]
-        for r in rated:
-            m.append(round(r.mu,3 ))
-            s.append(round(r.sigma,3))
-        self.extracted.append(m)
-        self.extracted.append(s)
+    def _download_ratings(self) -> None:
+        self.log.info('Download ranking...')
+        db: DBHandler = DBHandler()
+        [summoner.download_rating(db) for summoner in self.summoners]
+        del db
 
-    def rate_performance(self):
-        self.log.info('Rating participants')
-        x = list(zip(*self.extracted)) 
-        x.sort(key=lambda i: i[-7], reverse=True)
-        rating_group = []
-        for i in x:
-            row = (trueskill.Rating(i[-2], i[-1]),)
-            rating_group.append(row)
-        rated = trueskill.rate(rating_group)
-        m, s = [],[]
-        for r in rated:
-            m.append(round(r[0].mu,3 ))
-            s.append(round(r[0].sigma,3))
-        self.extracted = list(zip(*x))
-        self.extracted.append(m)
-        self.extracted.append(s)
+    def _rate_mmr(self) -> None:
+        self.summoners.sort(key=lambda x: x.win, reverse=True)
+        ratings: list[Rating] = [
+            Rating(s.old_mmr_mu, s.old_mmr_si) for s in self.summoners]
+        winners = ratings[:5]
+        losers = ratings[5:]
+        new_ratings = rate([winners, losers])
+        new_ratings = new_ratings[0]+new_ratings[1]
+        for idx, rating in enumerate(new_ratings):
+            self.summoners[idx].new_mmr_mu = rating.mu
+            self.summoners[idx].new_mmr_si = rating.sigma
 
+    def _rate_mvp(self) -> None:
+        self.summoners.sort(key=lambda x: x.win_prob, reverse=True)
+        rating_group: list[list[Rating]] = [
+            [Rating(s.old_mvp_mu, s.old_mvp_si)] for s in self.summoners]
+        new_ratings = rate(rating_group)
+        for idx, rating in enumerate(new_ratings):
+            self.summoners[idx].new_mvp_mu = rating[0].mu
+            self.summoners[idx].new_mvp_si = rating[0].sigma
+            
+    def _calculate_win_probability(self) -> None:
+        self.log.info('Calculating win probability')
+        for summoner in self.summoners:
+            summoner.win_prob = self.model.predict([summoner.stats])[0,0]
 
-    def update_ratings(self):
-        self.log.info('Updating ratings...')
-        db = dbhandler.DBHandler()
-        for i in range(10):
-            name: str = self.extracted[1][i]
-            puuid: str = self.extracted[0][i]
-            p_mu: float = self.extracted[-2][i]
-            p_sigma: float = self.extracted[-1][i]
-            m_mu: float = self.extracted[-4][i]
-            m_sigma: float = self.extracted[-3][i]
-            db.update_rating(name, puuid, m_mu, m_sigma, p_mu, p_sigma)
+    def show_participants(self) -> None:
+        self.summoners.sort(key=lambda x: x.win_prob, reverse=True)
+        [print(p) for p in self.summoners]
+
+    def commit(self) -> None:
+        db = DBHandler()
+        for summoner in self.summoners:
+            db.update_rating(
+                summoner.name, 
+                summoner.puuid, 
+                summoner.new_mmr_mu, 
+                summoner.new_mmr_si,
+                summoner.new_mvp_mu,
+                summoner.new_mvp_si,)
         db.set_match_as_analyzed(self.data['metadata']['matchId'])
         del db
 
-    def show_details(self):
-        for i in range(10):
-            name = self.extracted[1][i]
-            champion = self.extracted[2][i]
-            old_mu = self.extracted[-6][i]
-            old_sigma = self.extracted[-5][i]
-            mu = self.extracted[-2][i]
-            sigma = self.extracted[-1][i]
-            old_rating = round((old_mu - 3*old_sigma)*100)
-            rating = round((mu - 3*sigma)*100)
-            msg = f'{i+1}.\t{old_rating}\t->\t{rating}\t{name}\t{champion}'
-            print(msg)
-        x = list(zip(*self.extracted))
-        x = sorted(x, key=lambda y: y[3], reverse=True)
-        self.extracted = list(zip(*x))
-        print()
-        for i in range(10):
-            name = self.extracted[1][i]
-            champion = self.extracted[2][i]
-            old_mu = self.extracted[-8][i]
-            old_sigma = self.extracted[-7][i]
-            old_rating = round((old_mu - 3*old_sigma)*100)
-            mu = self.extracted[-4][i]
-            sigma = self.extracted[-3][i]
-            rating = round((mu - 3*sigma)*100)
-            msg = f'{i+1}.\t{old_rating}\t->\t{rating}\t{name}\t{champion}'
-            print(msg)
+
+class Summoner:
+    new_mmr_mu = None
+    new_mmr_si = None
+    new_mvp_mu = None
+    new_mvp_si = None
+    old_mmr_mu = None
+    old_mmr_si = None
+    old_mvp_mu = None
+    old_mvp_si = None
+    win_prob = None
+    
+    def __init__(self, data) -> None:
+        self.puuid = data[0]
+        self.name = data[1]
+        self.champion_name = data[2]
+        self.win = data[3]
+        self.stats = data[4:]
+
+    def __str__(self) -> str:
+        prob = ''
+        if self.win_prob:
+            prob = round(self.win_prob*100,2)
+        win = 0
+        if self.win:
+            win = 1
+        msg = f'{win} | {prob}%\t{self.name} - {self.champion_name}'
+        # new_mmr = round(100*(self.new_mmr_mu - 3*self.new_mmr_si))
+        # new_mvp = round(100*(self.new_mvp_mu - 3*self.new_mvp_si))
+        # old_mmr = round(100*(self.old_mmr_mu - 3*self.old_mmr_si))
+        # old_mvp = round(100*(self.old_mvp_mu - 3*self.old_mvp_si))
+        # msg += f'\n\t\t\tMMR: {old_mmr} -> {new_mmr}\n'
+        # msg += f'\t\t\tMVP: {old_mvp} -> {new_mvp}'
+        return msg
+
+    def download_rating(self, db) -> None:
+        rating: list[float] = db.get_rating(self.name, self.puuid)
+        self.old_mmr_mu = rating[0]
+        self.old_mmr_si = rating[1]
+        self.old_mvp_mu = rating[2]
+        self.old_mvp_si = rating[3]
+
+def main() -> None:
+    logging.basicConfig(level=logging.INFO)
+    match_data = get_last_match_data_by_summoner_name('Norcia')
+    target: Match = Match(match_data)
+    target.show_participants()
+
+if __name__ == '__main__':
+    main()
